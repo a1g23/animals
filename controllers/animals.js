7745137fedbe69f959efff1d8482befae87ff0d9
Alex Greenberg <acgreenberg2@gmail.com>
2024-01-05 19:36:00 -0500

    minimum viable product completed

--- controllers/animals.js
+++ controllers/animals.js
@@ -110,11 +110,67 @@ router.post("/", async (req, res) => {
     }
 })
 
+// edit route (get "/:id/edit" to a edit.ejs form)
+
+router.get("/:id/edit", async (req, res) => {
+    try {
+        // get the id
+        const id = req.params.id
+
+        // find the animal in db
+        const indyAnimal = await Animal.findById(id)
+
+        // send the indyAnimal to the edit.ejs
+        res.render("edit.ejs", {indyAnimal})
 
 
+    } catch(error) {
+    console.log(error.message)
+    }
+})
+
+// update route (PUT to "/:id" send to db and redirect to )
+
+router.put("/:id", async (req, res) => {
+    try {
+        // handle the booelean
+        req.body.extinct = req.body.extinct === "on" ? false : true
+
+        // find the id
+        const id = req.params.id
+
+        // edit animal
+        const updatedAnimal = req.body
+
+        // change the database
+        await Animal.findByIdAndUpdate(id, updatedAnimal)
+
+        // redirect to the animal that was edited
+        res.redirect(`/animals/${id}`)
+
+    } catch(error) {
+    console.log(error.message)
+    }
+})
+
+// delete route (Delete to "/:id remove from db and redirect")
+
+router.delete("/:id", async (req, res) => {
+    try {
+        // find the id
+        const id = req.params.id
+
+        // delete from the database
+        await Animal.findByIdAndDelete(id)
+
+        //redirect to main
+        res.redirect("/animals")
 
 
-
+    } catch(error) {
+    console.log(error.message)
+    }
+})
 
 
 // show route (get -> /:id)
